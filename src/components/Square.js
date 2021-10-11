9b8c0b422eb9ec6df6b9179cde0b7691da3cfdc7
islakhunnurdzaki22 <islakhunnurdzaki22@gmail.com>
2021-10-12 00:09:57 +0700

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+import cancel from '../images/cancel.png';
+import dot from '../images/dot.png'
+function Square({ val, chooseSquare }) {
+  return (
+    <div className="square" onClick={chooseSquare}>
+      {
+          val.toLowerCase() === 'o' ? (
+              <img src={dot} width={40} height={40} alt=""/>
+          ): (
+            val.toLowerCase() === 'x' ?(
+                <img src={cancel} width={40} height={40} alt=""/>
+            ): ''
+          )
+      }
+    </div>
+  );
+}
+
+export default Square;
